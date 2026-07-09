3f3afd98895e5668ac6e2152f2e35cf08bca569e
Djangodv <dd.manders@protonmail.com>
2026-07-09 08:30:14 +0200

    feat: added JavaScript for sidebar functionality

---
+++ assets/scripts/main.js
@@ -0,0 +1,4 @@
+// assets/scripts/main.js
+import * as sideNav from '../../src/components/sideNav.js'
+
+sideNav.displayEntries()
---
+++ src/components/sideNav.js
@@ -0,0 +1,34 @@
+import * as githubApi from "../services/githubApi.js";
+import * as mainView from "../components/mainView.js";
+
+const DIRECTORY = 'markdown-example'
+
+// Default export (only one per module) can be named anything you'd like
+// export default async function sideNav() {
+// }
+
+export async function displayEntries() {
+
+    const fileNav = document.getElementById('file-nav');
+    const apiData = await githubApi.retrieveData(DIRECTORY);
+
+    apiData.forEach(entry => {
+
+        const buttonElement = document.createElement('button');
+        buttonElement.textContent = entry.name;
+        buttonElement.setAttribute('file', entry.path);
+
+        fileNav.appendChild(buttonElement);
+
+        registerClickEvent(buttonElement, entry);
+
+        // console.log(entry); // Debug
+    });
+    
+};
+
+function registerClickEvent(element, entry) {
+    element.addEventListener('click', () => {
+        console.log('Entry clicked');
+    });
+};
